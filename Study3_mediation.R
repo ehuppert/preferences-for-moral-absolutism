9b0342b9d170f0f26eeea5466bdb5dcbf093315e
Elizabeth Huppert <elizabethhuppert@ECON-20078L.local>
2021-01-14 17:32:03 -0600

    study convention updates.

--- Study3_mediation.R
+++ Study3_mediation.R
@@ -11,35 +11,35 @@ library(psych)
 
 #read in data
 
-s2 <- read.csv(here("HypocrisyStudy2_cleaned.csv"))
+s3 <- read.csv(here("HypocrisyStudy3_cleaned.csv"))
 
 
 #Create composite variables
 
 #Hypocrisy
-hypocrisy2_check <- data.frame(s2$Hypocrisy_1, s2$Hypocrisy_2, s2$Hypocrisy_3, s2$Hypocrisy_4, s2$Hypocrisy_5)
-summary(hypocrisy2_check)
-describe(hypocrisy2_check)
-psych::alpha(hypocrisy2_check) # raw alpha = 0.92
-s2 <- mutate(s2, 
-             Hypocrisy_composite2 = ((s2$Hypocrisy_1 + s2$Hypocrisy_2 + s2$Hypocrisy_3 + s2$Hypocrisy_4 + s2$Hypocrisy_5)/5))
+hypocrisy3_check <- data.frame(s3$Hypocrisy_1, s3$Hypocrisy_2, s3$Hypocrisy_3, s3$Hypocrisy_4, s3$Hypocrisy_5)
+summary(hypocrisy3_check)
+describe(hypocrisy3_check)
+psych::alpha(hypocrisy3_check) # raw alpha = 0.92
+s3 <- mutate(s3, 
+             Hypocrisy_composite3 = ((s3$Hypocrisy_1 + s3$Hypocrisy_2 + s3$Hypocrisy_3 + s3$Hypocrisy_4 + s3$Hypocrisy_5)/5))
 
 #Morality  
-moral2_check <- data.frame(s2$Moral_1, s2$Moral_2, s2$Moral_3)
-summary(moral2_check)
-describe(moral2_check)
-psych::alpha(moral2_check) # raw = .96
-s2 <- mutate(s2, 
-             Moral_composite2 = ((s2$Moral_1+s2$Moral_2+s2$Moral_3)/3))
+moral3_check <- data.frame(s3$Moral_1, s3$Moral_2, s3$Moral_3)
+summary(moral3_check)
+describe(moral3_check)
+psych::alpha(moral3_check) # raw = .96
+s3 <- mutate(s3, 
+             Moral_composite3 = ((s3$Moral_1+s3$Moral_2+s3$Moral_3)/3))
 
 
 #Future Honesty 
-honest2_check <- data.frame(s2$Honest_frequency, s2$Honest_likely, s2$Honest_extreme, s2$Honest_committment)
-summary(honest2_check)
-describe(honest2_check)
-psych::alpha(honest2_check) # rawalpha = 0.84
-s2<- mutate(s2, 
-            Honest_composite2 = ((s2$Honest_frequency + s2$Honest_likely + s2$Honest_extreme + s2$Honest_committment)/4))
+honest3_check <- data.frame(s3$Honest_frequency, s3$Honest_likely, s3$Honest_extreme, s3$Honest_committment)
+summary(honest3_check)
+describe(honest3_check)
+psych::alpha(honest3_check) # rawalpha = 0.84
+s3<- mutate(s3, 
+            Honest_composite3 = ((s3$Honest_frequency + s3$Honest_likely + s3$Honest_extreme + s3$Honest_committment)/4))
 
 
 
@@ -47,16 +47,16 @@ s2<- mutate(s2,
 
 #IV = Sometimes = dummy-coded proclamation variable (sometimes = 1, rarely/never = 0)
 
-#Mediators: future honesty (Honest_composite2), hypocrisy (Hypocrisy_composite2)
+#Mediators: future honesty (Honest_composite3), hypocrisy (Hypocrisy_composite3)
 
 #Moderator = Behave lie 1 = prosocial lie, 0 = selfish truth
 
-#DV: morality (Moral_composite2)
+#DV: morality (Moral_composite3)
 
 med_mod_study2 = "
-Hypocrisy_composite2  ~ a11*sometimes + a12*behavlie + a13*sometimes:behavlie
-Honest_composite2~ a21*sometimes + a22*behavlie+ a23*sometimes:behavlie
-Moral_composite2 ~ b11*Hypocrisy_composite2 + b21*Honest_composite2 + c1*sometimes
+Hypocrisy_composite3  ~ a11*sometimes + a12*behavlie + a13*sometimes:behavlie
+Honest_composite3~ a21*sometimes + a22*behavlie+ a23*sometimes:behavlie
+Moral_composite3 ~ b11*Hypocrisy_composite3 + b21*Honest_composite3 + c1*sometimes
 
 #Indirect effects conditional on moderator (a11+a13*ModValue)*b11 + (a21+a23*ModValue)*b21 
 
@@ -87,9 +87,9 @@ prop_mediated_prosocial := indirect_prosocial / total_prosocial
 "
 
 
-fit.Mod.Med.study2 <- sem(model = med_mod_study2, se = "boot", bootstrap = 1000, data = s2, 
+fit.Mod.Med.study3 <- sem(model = med_mod_study3, se = "boot", bootstrap = 1000, data = s3, 
                                likelihood = "wishart")
-summary(fit.Mod.Med.study2, standardized = T, rsq = T)
-parameterEstimates((fit.Mod.Med.study2))
+summary(fit.Mod.Med.study3, standardized = T, rsq = T)
+parameterEstimates((fit.Mod.Med.study3))
 
 #Note: see SPSS output for moderated mediation on trust.
